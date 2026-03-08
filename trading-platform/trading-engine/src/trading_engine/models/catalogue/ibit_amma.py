b13301c3c6b12ef18ecb81b6ef6a610ba0fbcb99
Sam(Chaoyue) Geng <SamGeng14@gmail.com>
2026-03-07 22:52:19 -0600

    Reset repo and add clean project structure

--- trading-platform/trading-engine/src/trading_engine/models/catalogue/ibit_amma.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-from . import amma
-
-
-def generate_positions(data: pd.DataFrame) -> pd.DataFrame:
-    pos = amma.position(data)
-    return pd.DataFrame({"date": data.index, "IBIT-US": pos})
